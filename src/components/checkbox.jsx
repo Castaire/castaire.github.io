01702443e59ba48710182f903157781fc2c25023
Castaire <iv.castaire@gmail.com>
2023-04-17 16:58:30 -0700

    working basic custom checkboxes; new isotope dependency

---
+++ src/components/checkbox.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function CheckBox({fillColorClass, borderColorClass, caption, defaultClick}) {
+    const [clicked, setClicked] = useState({defaultClick});
+
+    function flipClicked() {
+        setClicked(!clicked);
+    }
+
+    function getColorClasses() {
+        return clicked ? `${fillColorClass} ${borderColorClass}` : `${borderColorClass}`;
+    }
+
+    return (
+        <div className="checkbox">
+            <input type="checkbox"/>
+            <label onClick={flipClicked}
+                className={getColorClasses()}/>
+            <p>{caption}</p>
+        </div>
+    );
+}
+
+export default CheckBox;
