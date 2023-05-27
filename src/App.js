eeeb9bc96485cd2574b53a035c20b9421ef66882
Castaire <iv.castaire@gmail.com>
2023-05-26 20:32:56 -0700

    toggle sort checkbox

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   const [showWork, flipShowWork] = useState(true);
   const [showProject, flipShowProject] = useState(true);
   const [sortKey, setSortKey] = useState('date'); // 'date' or 'name'
-  
+
   // setup isotope
   const isotope = useRef(); // persist isotope in between renders
-  
+
   useEffect(() => {
     isotope.current = new Isotope(".card-container", {
       itemSelector: ".card",
@@ -27,18 +27,18 @@ function App() {
         }
       }
     });
-    
+
     return () => {
       isotope.current.destroy();
     };
   }, []);
-  
+
   useEffect(() => {
     var options = {
       filter: '*',
       sortBy: sortKey
     };
-    
+
     if (!showWork && !showProject) {
       options.filter = ':not(.card)';
     } else if (showWork && !showProject) {
@@ -46,86 +46,91 @@ function App() {
     } else if (showProject && !showWork) {
       options.filter = '.project';
     }
-    
+
     if (sortKey == 'date') {
       options.sortAscending = false;
     } else if (sortKey == 'name') {
       options.sortAscending = true;
     }
-    
+
     isotope.current.arrange(options);
-    
+
   }, [isotope, showWork, showProject, sortKey]);
-  
-  
-  
+
   // create cards
   let data = require("./data/card_data.json");
-  
+
   let work_cards = data.work_cards.map(function (wc, index) {
     return (
       <WorkCard
-      date={wc.date}
-      title={wc.title}
-      company={wc.company}
-      description={wc.description}
-      key={index}
+        date={wc.date}
+        title={wc.title}
+        company={wc.company}
+        description={wc.description}
+        key={index}
       />
-      )
-    });
-    
-    let proj_cards = data.project_cards.map(function(pc, index) {
-      return (
-        <ProjectCard
+    )
+  });
+
+  let proj_cards = data.project_cards.map(function (pc, index) {
+    return (
+      <ProjectCard
         date={pc.date}
         name={pc.name}
         description={pc.description}
         links={pc.links}
         details={pc.details}
         key={index}
-        />
-        )
-      });
-      
-      return (
-        <>
-        <div id="filterbar">
+      />
+    )
+  });
+
+  let by_date_checkbox = <CheckBox
+    caption="DATE"
+    clicked={sortKey === 'date'}
+    onClick={() => {
+      setSortKey('date');
+    }}
+    fillColorClass={"charcoalfill"}
+    borderColorClass={"charcoalborder"}
+  />
+
+  let by_name_checkbox = <CheckBox
+    caption="NAME"
+    clicked={sortKey === 'name'}
+    onClick={() => {
+      setSortKey('name');
+    }}
+    fillColorClass={"charcoalfill"}
+    borderColorClass={"charcoalborder"}
+  />
+
+  return (
+    <>
+      <div id="filterbar">
         <CheckBox
-        caption="PROJECTS"
-        clicked={showProject}
-        onClick={() => flipShowProject(!showProject)}
-        fillColorClass={"whalebluefill"}
-        borderColorClass={"whaleblueborder"}
+          caption="PROJECTS"
+          clicked={showProject}
+          onClick={() => flipShowProject(!showProject)}
+          fillColorClass={"whalebluefill"}
+          borderColorClass={"whaleblueborder"}
         />
         <CheckBox
-        caption="WORK"
-        clicked={showWork}
-        onClick={() => flipShowWork(!showWork)}
-        fillColorClass={"brickredfill"}
-        borderColorClass={"brickredborder"}
+          caption="WORK"
+          clicked={showWork}
+          onClick={() => flipShowWork(!showWork)}
+          fillColorClass={"brickredfill"}
+          borderColorClass={"brickredborder"}
         />
-        <CheckBox
-        caption="BY DATE"
-        clicked={sortKey === "date"}
-        onClick={() => setSortKey('date')}
-        fillColorClass={"charcoalfill"}
-        borderColorClass={"charcoalborder"}
-        />
-        <CheckBox
-        caption="BY NAME"
-        clicked={sortKey === "name"}
-        onClick={ () => setSortKey('name')}
-        fillColorClass={"charcoalfill"}
-        borderColorClass={"charcoalborder"}
-        />
-        </div>
-        <div className="card-container">
-          {work_cards}
-          {proj_cards}
-        </div>
-        </>
-        );
-      }
-      
-      export default App;
-      
+        {by_date_checkbox}
+        {by_name_checkbox}
+      </div>
+      <div className="card-container">
+        {work_cards}
+        {proj_cards}
+      </div>
+    </>
+  );
+}
+
+export default App;
--- src/components/project_card.jsx
+++ src/components/project_card.jsx
@@ -1,55 +1,56 @@
 import { useState } from "react";
 
-function ProjectCard({date, name, description, links, details}) {
-    const [showDetails, setDetails] = useState(false);
-    
-    function flipDetails() {
-        setDetails(!showDetails);
-    }
+function ProjectCard({ date, name, description, links, details }) {
+  const [showDetails, setDetails] = useState(false);
 
-    function getLinks() {
-        return (
-            links.map(link => 
-                <>
-                <a className={"finetext"} href={link.href} target="_blank" rel="noreferrer">
-                    // {link.description}
-                </a>
-                <br/>
-                </>
-            )
-        );
+  function flipDetails() {
+    setDetails(!showDetails);
+  }
+
+  function getLinks() {
+    return links.map((link) => (
+      <>
+        <a
+          className={"finetext"}
+          href={link.href}
+          target="_blank"
+          rel="noreferrer"
+        >
+          // {link.description}
+        </a>
+        <br />
+      </>
+    ));
+  }
+
+  function getCardContent() {
+    if (!showDetails) {
+      return (
+        <>
+          <h3>{name}</h3>
+          <p>{description}</p>
+        </>
+      );
+    } else {
+      let linksJsx = getLinks();
+      return (
+        <>
+          <br />
+          <p>{details}</p>
+          {linksJsx}
+        </>
+      );
     }
-    
-    function getCardContent() {
-        if (!showDetails) {
-            return (
-                <>
-                    <h3>{name}</h3>
-                    <p>{description}</p>
-                </>
-            );
-        } else {
-            let linksJsx = getLinks();
-            return (
-                <>
-                    <br/>
-                    <p>{details}</p>
-                    {linksJsx}
-                </>
-            );
-        }    
-    }
-    
-    return (
-        <div className="card project" date={date} name={name}>
-            <button onClick={flipDetails}>
-                <p>//</p>
-            </button>
-            <div className="text">
-                {getCardContent()}
-            </div>
-        </div>
-    );
+  }
+
+  return (
+    <div className="card project" date={date} name={name}>
+      <button onClick={flipDetails}>
+        <p>//</p>
+      </button>
+      <div className="text">{getCardContent()}</div>
+    </div>
+  );
 }
-        
+
 export default ProjectCard;
